7e3dabff52ba68ad1583317415529fa0177855ea
Vânia Almeida <vania.almeida28@hotmail.com>
2020-08-07 06:00:09 -0300

    criando a rota das classes e inserindo os dados

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,9 @@
 import express from "express";
+import routes from "./routes";
 
 const app = express();
 
 app.use(express.json());
-
-app.get("/users", (request, response) => {
-  console.log(request.query);
-
-  const users = [
-    { name: "Vania", age: 30 },
-    { name: "Diego", age: 43 },
-  ];
-
-  return response.json(users);
-});
+app.use(routes);
 
 app.listen(3333);
